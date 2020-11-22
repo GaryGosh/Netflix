2ff3818c83ae781837c36f7fedcefe0641e04151
GaryGosh <clichethe1@yahoo.com>
2020-11-23 00:29:00 +0530

    Updates

---
+++ precache-manifest.5e1bfdbd4e491ab46adba3635a83e33a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a765c06796acaa12720529b388798089",
+    "url": "/Netflix/index.html"
+  },
+  {
+    "revision": "51dc20c8ab61688fcd96",
+    "url": "/Netflix/static/css/main.89dba012.chunk.css"
+  },
+  {
+    "revision": "59afc0e41a3c10a37df2",
+    "url": "/Netflix/static/js/2.4b6a2cfc.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/Netflix/static/js/2.4b6a2cfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51dc20c8ab61688fcd96",
+    "url": "/Netflix/static/js/main.126a7764.chunk.js"
+  },
+  {
+    "revision": "1111f8ae901435215b7d",
+    "url": "/Netflix/static/js/runtime-main.17f22a80.js"
+  }
+]);
